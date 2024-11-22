b407226dfc344b194209453c2c332c5c69cf7e45
Abdul Moiz <abdulmoizofficial0@gmail.com>
2024-11-23 01:00:38 +0500

    metadata_download functions completed.

--- metadata/metadata.py
+++ metadata/metadata.py
@@ -29,7 +29,12 @@ def metadata_download(source_id):
     conn = db_conn()
     cur = conn.cursor()
     classes = []
-    class_data = []
+    property_data = []
+    member_data = []
+    office_data = []
+    media_data = []
+    openhouse_data = []
+    room_data = []
 
     get_api = '''SELECT auth->>'$.loginUrl' AS api_url FROM sources WHERE source_id = %s;'''
     get_key = '''SELECT auth->>'$.password' AS api_key FROM sources WHERE source_id = %s;'''
@@ -47,24 +52,46 @@ def metadata_download(source_id):
     resp = requests.get(url=api_url[0], headers=header)
     xml_data = resp.text
     root = ET.fromstring(xml_data)
-    entity_types = root.findall('.//{http://docs.oasis-open.org/odata/ns/edm}EntityType')
+    namespace = {'edm': 'http://docs.oasis-open.org/odata/ns/edm'}
+    entity_types = root.findall('.//edm:EntityType', namespace)
 
     for entity_type in entity_types:
         entity_name = entity_type.get('Name')
         classes.append(entity_name)
-
-        entries = root.findall(f'.//{entity_name}')
-
-        for entry in entries:
-            for prop in entry:
+        properties = entity_type.findall('edm:Property', namespace)
+        if entity_name == classes[0]:
+            for prop in properties:
                 prop_name = prop.get('Name')
-                prop_value = prop.text
-                class_data.append(prop_value)
-    print(class_data)
+                property_data.append(prop_name)
+        elif entity_name == classes[1]:
+            for prop in properties:
+                prop_name = prop.get('Name')
+                member_data.append(prop_name)
+        elif entity_name == classes[2]:
+            for prop in properties:
+                prop_name = prop.get('Name')
+                office_data.append(prop_name)
+        elif entity_name == classes[3]:
+            for prop in properties:
+                prop_name = prop.get('Name')
+                media_data.append(prop_name)
+        elif entity_name == classes[4]:
+            for prop in properties:
+                prop_name = prop.get('Name')
+                openhouse_data.append(prop_name)
+        elif entity_name == classes[5]:
+            for prop in properties:
+                prop_name = prop.get('Name')
+                room_data.append(prop_name)
+    return classes, property_data, member_data, office_data, media_data, openhouse_data, room_data
 
-
+def insertion(conn, classes):
+    conn = db_conn()
+    cur = conn.cursor()
+    pass
 
 db = db_conn()
-metadata_download(897)
+insertion(metadata_download(897))
+
 
 
